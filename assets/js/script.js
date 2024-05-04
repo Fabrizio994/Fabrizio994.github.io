47916be09a852832fbc32a78f032acf613693584
Fabrizio Recchia <fabrizio643@hotmail.it>
2024-05-04 11:26:16 +0200

    Aggiunta Sticky menu/bootstrap/cambio repo/aggiunta css/aggiunta og/curriculum/FabrizioDev/divisionehtml

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,20 @@
-
-window.onload = function() {
-  var contact_service = 'service_hm608dy';
-  var contact_form = 'template_mgle077';
-  document.getElementById('contact-form').addEventListener('submit', function(event) {
+window.onload = function () {
+  var contact_service = "service_hm608dy";
+  var contact_form = "template_mgle077";
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
       event.preventDefault();
       // these IDs from the previous steps
-      emailjs.sendForm(contact_service, contact_form, this)
-          .then(() => {
-              console.log('SUCCESS!');
-              alert('MAIL INVIATA CON SUCCESSO! GRAZIE PER AVERMI CONTATTATO')
-          }, (error) => {
-              console.log('FAILED...', error);
-              alert('OPS.. QUALCOSA E ANDATO STORTO');
-          });
-  });
-}
+      emailjs.sendForm(contact_service, contact_form, this).then(
+        () => {
+          console.log("SUCCESS!");
+          alert("MAIL INVIATA CON SUCCESSO! GRAZIE PER AVERMI CONTATTATO");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          alert("OPS.. QUALCOSA E ANDATO STORTO");
+        }
+      );
+    });
+};
